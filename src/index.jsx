c9d3224521064303ec534741df756aa9418acbda
sballay8734 <48398020+sballay8734@users.noreply.github.com>
2023-02-17 07:09:56 -0500

    React Router Implementation

--- src/index.jsx
+++ src/index.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 
 const App = () => {
   return (
+    // Browser Router implementation
     <BrowserRouter>
       <MainView />
     </BrowserRouter>
